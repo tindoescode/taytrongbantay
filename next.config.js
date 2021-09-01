b4a1f712b58f174aff3d79aa1be5c8d134d06c21
Tin Trung Nguyen <hopthucuatin@gmail.com>
2021-09-01 22:37:02 +0700

    Fix showing post

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@ module.exports = {
   reactStrictMode: true,
   env: {
     mongodburl: "mongodb+srv://tinisntreal:8ZILbgkqoPGUuM84@cluster0.iukas.mongodb.net/taytrongvongtay?retryWrites=true&w=majority",
-    JWT_SECRET: "Ygtc#+TM6f__2Nb2sanlasfnsafasjfasjb"
-  }
+    JWT_SECRET: "Ygtc#+TM6f__2Nb2sanlasfnsafasjfasjb",
+    baseUrl: 'http://localhost:3000'
+  },
+  images: {
+    domains: ['i.imgur.com'],
+  },  
 
 }
