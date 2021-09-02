70a663a72606043fd79506f7eeaf53232cee606f
Tin Trung Nguyen <hopthucuatin@gmail.com>
2021-09-02 14:22:23 +0700

    Add skeleton loading

--- components/NewPosts.js
+++ components/NewPosts.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import PostCard from '../components/PostCard';
 import Title from '../components/Title';
 import axios from 'axios';
+import Skeleton from 'react-loading-skeleton';
 
 export default function NewPosts() {
   let [posts, setPosts] = useState([]);
@@ -16,6 +17,7 @@ export default function NewPosts() {
 
   return (<div className="col-span-2 shadow-md ring-1 ring-green-200">
         <Title>Bài mới</Title>
+        {!posts && <Skeleton count={5} height={100} />}
         {posts.map((post, index) => {
         return <PostCard key={index} href={process.env.baseUrl + '/posts/' + post.slug} title={post.title} description={post.description} />
         })}
