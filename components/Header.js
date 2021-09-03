4b8954ca24a4f4ae95f5b1fc5d20e410c3e05e9b
Tin Trung Nguyen <hopthucuatin@gmail.com>
2021-09-03 21:45:23 +0700

    Fix login

--- components/Header.js
+++ components/Header.js
@@ -71,28 +71,28 @@ function Header() {
   }, []);
 
   
-  const checkLoginState = (res) => {
-    console.log('checkLoginState', res);
-    console.log('access_token', res.authResponse.access_token);
+  const checkLoginState = async (res) => {
+    // console.log('checkLoginState', res);
+    // console.log('access_token', res);
+    let access_token = res.authResponse.access_token;
 
-    axios.post('/api/auth/facebook', { token: res.authResponse.access_token }).then((response) => {
-      console.log('fbID', response.user.facebookId);
-      if(res.authResponse.userId === response.user.facebookId) {
-        let user = response.user;
-        dispatch({
-          type: 'ON_LOGIN',
-          user
-        });
-  
-        toast(`Chúc ${user.name} có một ngày vui vẻ!`)
-      }
-      else {
-        toast.error(`Đăng nhập thất bại!`)
-      }
-    })
-
-
-
+    setTimeout(() => {
+      axios.post('/api/auth/facebook', { token: access_token }).then((response) => {
+        console.log('fbID', response.user.facebookId);
+        if(res.authResponse.userId === response.user.facebookId) {
+          let user = response.user;
+          dispatch({
+            type: 'ON_LOGIN',
+            user
+          });
+    
+          toast(`Chúc ${user.name} có một ngày vui vẻ!`)
+        }
+        else {
+          toast.error(`Đăng nhập thất bại!`)
+        }
+      })
+    }, 2000)
   }
 
   const facebookLoginButtonCLick = (e) => {
