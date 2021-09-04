ffd463584e050780181a3c12f104cc0c593e4e10
Tin Trung Nguyen <hopthucuatin@gmail.com>
2021-09-04 12:04:04 +0700

    fix header

--- components/Header.js
+++ components/Header.js
@@ -89,13 +89,13 @@ function Header() {
 
   return (<>
     <div className="flex shadow-md md:justify-around fixed w-screen top-0 bg-white z-50">
-      <div className="flex-grow md:flex-grow-0 ml-2">
+      <div className="ml-2">
         <Link href='/'>
           <a><div className="Header--logo" /></a>
         </Link>
       </div>
 
-      <div className="Navbar mr-2">
+      <div className="flex-grow justify-end md:flex-grow-0 Navbar mr-2">
         <div className="Navbar--item hover:bg-green-300 hover:text-white transition md:relative">
           <a onClick={() => toggleLoginMenu()}><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-person" viewBox="-2 -5 20 20" preserveAspectRatio="xMidYMid slice">
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
