c358b60681dc916b1182f654a292d4825cfdcf63
Tin Nguyen <40380704+tindoescode@users.noreply.github.com>
2021-09-07 18:47:01 +0700

    Update Title.js

--- components/Title.js
+++ components/Title.js
@@ -1,6 +1,6 @@
 const Title = ({children, style = 'green'}) => {
     return (
-        <div className={`p-2 bg-${style}-400 text-white font-bold ring-1 ring-${style}-400`}>
+        <div className={`p-2 bg-${style}-400 text-white font-bold border-1 border-green-400`}>
             {children}
         </div>
     )
